463c9c35139a6eec206eabfe7543d476bef0f4e6
Terry Qi <fortianrui4@gmail.com>
2022-04-21 17:37:45 +1200

    attempting build script

---
+++ build.js
@@ -0,0 +1,58 @@
+const {firefox} = require('playwright-firefox');
+const hserver = require('http-server');
+const fs = require("fs");
+const path = require("path");
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function* walk(dir) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
+
+(async () => {
+
+    // This script visits all files in the canvas/ directory, generates a mapping file, and screenshots the elements
+
+    // first start server at localhost:8080 in canvas/
+    hserver.createServer({root: '.'}).listen(8080);
+
+    const browser = await firefox.launch({headless: false});
+    const page = await browser.newPage();
+
+    // visit all files in canvas/
+    const mapping = {};
+    for await (const p of walk('./canvas/simulations')) {
+
+        // ignore not .html files
+        if (!p.endsWith('.html')) {
+            continue;
+        }
+
+        console.log(p);
+
+        // visit the file
+        // http://localhost:8080/canvas/simulations/shm.html
+        await page.goto(`http://localhost:8080/${p}`);
+        // wait for canvas#root to be loaded
+        await page.waitForSelector('canvas#root');
+        // wait half a second
+        await wait(500);
+        // take a screenshot of the canvas#root element
+        await page.locator('canvas#root').screenshot(
+            {
+                path: `./canvas/screenshots/${p.replace('./canvas/simulations/', '')
+                    .replace('.html', '.png')}`
+            });
+
+
+    }
+
+
+})();
